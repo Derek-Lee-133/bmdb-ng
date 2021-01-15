57ad57320b7890dfe86891f814cfafda8016832a
Derek Lee <dlee133@gmail.com>
2021-01-14 22:25:07 -0500

    added functions to actor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { NoComponentDefinedComponent } from './core/no-component-defined/no-component-defined.component';
 import { ActorCreateComponent } from './feature/actor/actor-create/actor-create.component';
 import { ActorDetailComponent } from './feature/actor/actor-detail/actor-detail.component';
+import { ActorEditComponent } from './feature/actor/actor-edit/actor-edit.component';
 import { ActorListComponent } from './feature/actor/actor-list/actor-list.component';
 import { CreditListComponent } from './feature/credit/credit-list/credit-list.component';
 import { MovieCreateComponent } from './feature/movie/movie-create/movie-create.component';
@@ -20,6 +21,7 @@ const routes: Routes = [
  {path: 'actor-list', component: ActorListComponent},
  {path: 'actor-create', component: ActorCreateComponent},
  {path: 'actor-detail/:id', component: ActorDetailComponent},
+ {path: 'actor-edit/:id', component: ActorEditComponent},
  {path: 'credit-list', component: CreditListComponent},
  {path: '**', component: NoComponentDefinedComponent}
 ];
--- src/app/feature/actor/actor-edit/actor-edit.component.ts
+++ src/app/feature/actor/actor-edit/actor-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Actor } from 'src/app/model/actor.class';
+import { ActorService } from 'src/app/service/actor.service';
 
 @Component({
   selector: 'app-actor-edit',
@@ -6,10 +9,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./actor-edit.component.css']
 })
 export class ActorEditComponent implements OnInit {
+  title = "Actor Edit";
+  actor: Actor = null;
+  actorId: number = 0;
+  submitBtnTitle = "Save";
 
-  constructor() { }
+  constructor(private actorSvc: ActorService,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+     // get the id from url
+     this.route.params.subscribe(
+      parms => {
+        this.actorId = parms['id'];
+        console.log(this.actorId);
+      }
+    );
+    this.actorSvc.getById(this.actorId).subscribe(
+      resp => {
+        this.actor = resp as Actor;
+        console.log('Actor', this.actor);
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  
+    }
+  save()  {
+    // save the actor to DB
+    this.actorSvc.update(this.actor).subscribe(
+      resp => {
+        this.actor = resp as Actor;
+        console.log('Actor updated' ,this.actor)
+        // forward to the movie list component
+          this.router.navigateByUrl("/actor-list");
+      },
+      err =>{
+        console.log(err);
+      }
+    );
   }
 
 }
